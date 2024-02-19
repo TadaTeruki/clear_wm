5fe7975934bb1595f0793c0871fa6b3ddb4cc620
Teruki TADA <me@peruki.dev>
2024-02-19 15:42:45 +0900

    add client/container

--- src/model/client/client.rs
+++ src/model/client/client.rs
@@ -3,8 +3,8 @@ pub struct Client<WinId>
 where
     WinId: Copy + PartialEq,
 {
-    app_id: WinId,
-    frame_id: WinId,
+    pub app_id: WinId,
+    pub frame_id: WinId,
 }
 
 impl<WinId> Client<WinId>
@@ -13,71 +13,5 @@ where
 {
     pub fn new(app_id: WinId, frame_id: WinId) -> Self {
         Self { app_id, frame_id }
-    }
-}
-
-pub struct ClientContainer<WinId>
-where
-    WinId: Copy + PartialEq,
-{
-    clients: Vec<Client<WinId>>,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum WindowType {
-    App,
-    Frame,
-}
-
-impl<WinId> ClientContainer<WinId>
-where
-    WinId: Copy + PartialEq,
-{
-    pub fn new() -> Self {
-        Self {
-            clients: Vec::new(),
-        }
-    }
-
-    pub fn add_client(&mut self, client: Client<WinId>) {
-        self.clients.push(client);
-    }
-
-    pub fn query_client_from_id(&self, win_id: WinId) -> Option<(&Client<WinId>, WindowType)> {
-        if let Some(app) = self.clients.iter().find(|client| client.app_id == win_id) {
-            return Some((app, WindowType::App));
-        } else if let Some(frame) = self.clients.iter().find(|client| client.frame_id == win_id) {
-            return Some((frame, WindowType::Frame));
-        }
-        None
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_client_container() {
-        let mut container = ClientContainer::new();
-        container.add_client(Client::new(1, 2));
-        container.add_client(Client::new(3, 4));
-        assert_eq!(
-            container.query_client_from_id(1).unwrap().1,
-            WindowType::App
-        );
-        assert_eq!(
-            container.query_client_from_id(2).unwrap().1,
-            WindowType::Frame
-        );
-        assert_eq!(
-            container.query_client_from_id(3).unwrap().1,
-            WindowType::App
-        );
-        assert_eq!(
-            container.query_client_from_id(4).unwrap().1,
-            WindowType::Frame
-        );
-        assert_eq!(container.query_client_from_id(5), None);
     }
 }
